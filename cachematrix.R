296c2bd4e7b0bd8fa717950990ca75e839e149d2
kamal811 <kamal.811@gmail.com>
2016-09-02 23:51:35 +0530

    Revert "Revert "Initial Submission of the file""
    This reverts commit 492d113cc8d151f46aebe8bec391db6b83ec3b34.

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <- NULL
+  
+  set <- function(y) 
+  { 
+    x <<- y  
+    m <<- NULL
+  }
+  
+  get <- function()  x
+  
+  setInvMatrix <- function(inverse)  m <<- inverse
+  getInvMatrix <- function()  m 
+  
+  list(set = set , get=get, setInvMatrix = setInvMatrix , getInvMatrix = getInvMatrix)
 
 }
 
@@ -12,4 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInvMatrix()
+  
+  if(!is.null(inv))
+  {
+    message("Getting data from cache")
+    return(inv)
+  }
+  
+  srcMatrix <- x$get()
+  inv <- solve(srcMatrix)
+  x$setInvMatrix(inv)
+  
+  message("Setting data into cache")
+  
+  inv
+  
 }
